2e38a59a26e6aaa7ba6fe8b45f3516d76135b784
Leykun Gizaw <leykungizaw@gmail.com>
2024-04-11 17:40:22 +0300

    feat(task 0): Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+def add(a: float, b: float) -> float:
+    """Adds two floating point numbers
+
+    Args:
+        a (float): First floating point number
+        b (float): Second floating point number
+
+    Returns:
+        Sum of `a` and `b`
+    """
+    return a + b
